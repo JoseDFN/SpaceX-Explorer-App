8534ead0c2074038a21a584f9d956a127f6b4b13
JoseDFN <63378897+JoseDFN@users.noreply.github.com>
2025-07-27 16:46:05 -0500

    feat: :sparkles: implement home screen dashboard UI
    Creates the UI for the home screen, featuring a vertically scrollable column with multiple horizontal carousels for different content sections like 'Latest Launches' and 'Rockets'.

--- app/src/main/java/com/jdf/spacexexplorer/presentation/navigation/Screen.kt
+++ app/src/main/java/com/jdf/spacexexplorer/presentation/navigation/Screen.kt
@@ -6,5 +6,8 @@ sealed class Screen(val route: String) {
     object LaunchDetail : Screen("launch_detail_screen/{launchId}") {
         fun createRoute(launchId: String) = "launch_detail_screen/$launchId"
     }
+    object RocketDetail : Screen("rocket_detail_screen/{rocketId}") {
+        fun createRoute(rocketId: String) = "rocket_detail_screen/$rocketId"
+    }
     // Add more screens as needed
 } 
